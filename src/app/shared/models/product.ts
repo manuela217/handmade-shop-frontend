7fcab69904493479b315f954af999e1b0bd84e88
Manuela Mendoza Barba <manuela.mendoza.dev@gmail.com>
2026-04-30 00:37:59 +0200

    Add product model

---
+++ src/app/shared/models/product.ts
@@ -0,0 +1,5 @@
+export class Product {
+    constructor(public id:number, public name:string, public code:string, public description:string, public urlImage:string, public image:File, public price:number, public userId:string, public categoryId:string) {
+        
+    }
+}

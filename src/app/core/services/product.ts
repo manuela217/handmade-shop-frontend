170c30ce689820581d20388e1f2f05dcd78481f4
Manuela Mendoza Barba <manuela.mendoza.dev@gmail.com>
2026-04-30 00:44:15 +0200

    Add product service for API communication

---
+++ src/app/core/services/product.ts
@@ -0,0 +1,28 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class Product {
+  private apiUrl:string = "http://localhost:8080/api/v1/admin/products";
+
+  constructor(private httpClient:HttpClient) {}
+
+  getProducts():Observable<Product[]> {
+    return this.httpClient.get<Product[]>(this.apiUrl);
+  }
+
+  createProduct(formData:any):Observable<any> {
+    return this.httpClient.post<Product>(this.apiUrl, formData);
+  }
+
+  deleteProductById(id:number):Observable<any> {
+    return this.httpClient.delete(this.apiUrl + "/" + id);
+  }
+
+  getProductById(id:number):Observable<Product> {
+    return this.httpClient.get<Product>(this.apiUrl + "/" + id);
+  }
+}

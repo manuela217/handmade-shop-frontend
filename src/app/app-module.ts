623a688f40411b44521cfc2ce0804ed15c818f25
Manuela Mendoza Barba <manuela.mendoza.dev@gmail.com>
2026-05-03 21:20:06 +0200

    Create shared navbar and footer components and refactor app layout structure

--- src/app/app-module.ts
+++ src/app/app-module.ts
@@ -5,9 +5,11 @@ import { AppRoutingModule } from './app-routing-module';
 import { App } from './app';
 import { HomeComponent } from './features/home/home.component';
 import { HttpClientModule } from '@angular/common/http';
+import { Navbar } from './shared/components/navbar/navbar.component';
+import { Footer } from './shared/components/footer/footer.component';
 
 @NgModule({
-  declarations: [App, HomeComponent],
+  declarations: [App, HomeComponent, Navbar, Footer],
   imports: [BrowserModule, AppRoutingModule, HttpClientModule],
   providers: [provideBrowserGlobalErrorListeners()],
   bootstrap: [App],

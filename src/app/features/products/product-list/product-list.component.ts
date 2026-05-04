a8089484c3867d301f561155ec72b33dc39f354e
Manuela Mendoza Barba <manuela.mendoza.dev@gmail.com>
2026-05-04 17:43:33 +0200

    Fetch and display products in admin list view

--- src/app/features/products/product-list/product-list.component.ts
+++ src/app/features/products/product-list/product-list.component.ts
@@ -14,7 +14,7 @@ export class ProductListComponent implements OnInit {
   constructor(private productService:ProductService) {}
 
   ngOnInit(): void {
-    
+    this.listProducts();
   }
 
   listProducts() {

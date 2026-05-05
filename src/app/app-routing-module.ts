693a635c8bd3733d98d9fecb699d5f233e98125d
Manuela Mendoza Barba <manuela.mendoza.dev@gmail.com>
2026-05-05 18:08:33 +0200

    Implement routing with admin product list view

--- src/app/app-routing-module.ts
+++ src/app/app-routing-module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './features/home/home.component';
+import { ProductListComponent } from './features/products/product-list/product-list.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'admin/products', component: ProductListComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],

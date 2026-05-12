15ea21fd504f81538f396b9eac22cf880edc49cc
Manuela Mendoza Barba <manuela.mendoza.dev@gmail.com>
2026-05-12 21:15:47 +0200

    Add routing to admin product creation view

--- src/app/app-routing-module.ts
+++ src/app/app-routing-module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './features/home/home.component';
 import { ProductListComponent } from './features/admin/products/product-list/product-list.component';
+import { ProductAddComponent } from './features/admin/products/product-add/product-add.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
-  { path: 'admin/products', component: ProductListComponent }
+  { path: 'admin/products', component: ProductListComponent },
+  { path: 'admin/products/add', component: ProductAddComponent }
 ];
 
 @NgModule({

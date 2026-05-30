52ac5524319c9f17b3ddc790e8f16ebceb0e8f58
Manuela Mendoza Barba <manuela.mendoza.dev@gmail.com>
2026-05-30 17:52:05 +0200

    Create category model and API service

---
+++ src/app/core/services/category.service.ts
@@ -0,0 +1,31 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Category } from '../../shared/models/category';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class CategoryService {
+
+  private apiUrl:string = 'http://localhost:8080/api/v1/admin/categories';
+
+  constructor(private http:HttpClient) {}
+
+  getCategoryList():Observable<Category[]> {
+    return this.http.get<Category[]>(this.apiUrl);
+  }
+
+  createCategory(category:Category):Observable<Category> {
+    return this.http.post<Category>(this.apiUrl,category);
+  }
+
+  deleteCategoryById(id:number):Observable<any> {
+    return this.http.delete(`${this.apiUrl}/${id}`);
+  }
+
+  getCategoryById(id:number):Observable<Category> {
+    return this.http.get<Category>(`${this.apiUrl}/${id}`);
+  }
+
+}

5d07d158209893ccd386cf60e58175c9f8bd623f
Manuela Mendoza Barba <manuela.mendoza.dev@gmail.com>
2026-06-23 19:13:59 +0200

    Implement shopping cart summary and item removal

--- src/app/features/cart/summary-order/summary-order.component.ts
+++ src/app/features/cart/summary-order/summary-order.component.ts
@@ -1,8 +1,36 @@
-import { Component } from '@angular/core';
+import { ChangeDetectorRef, Component, OnInit } from '@angular/core';
+import { ShoppingCart } from '../../../shared/models/shopping-cart';
+import { ShoppingCartService } from '../../../core/services/shopping-cart.service';
 
 @Component({
   selector: 'app-summary-order',
   standalone: false,
   templateUrl: './summary-order.component.html',
 })
-export class SummaryOrderComponent {}
+export class SummaryOrderComponent implements OnInit {
+
+  items:ShoppingCart [] = [];
+  totalCart:number = 0;
+  firstName:string = '';
+  lastName:string = '';
+  email:string = '';
+  address:string = '';
+
+  constructor(
+    private cartService:ShoppingCartService,
+    private cdr: ChangeDetectorRef,
+  ) {}
+
+  ngOnInit(): void {
+    this.items = this.cartService.convertToListFromMap();
+    this.totalCart = this.cartService.totalCart();
+  }
+
+  deleteItemCart(productId:number) {
+    this.cartService.deleteItemCart(productId);
+    this.items = this.cartService.convertToListFromMap();
+    this.totalCart = this.cartService.totalCart();
+
+    this.cdr.detectChanges();
+  }
+}

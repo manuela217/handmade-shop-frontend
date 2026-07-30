106ccea28d1feee2caac78f844869941ddc02bc0
Manuela Mendoza Barba <manuela.mendoza.dev@gmail.com>
2026-07-30 18:14:45 +0200

    Add logout option for authenticated users

--- src/app/shared/components/navbar/navbar.component.ts
+++ src/app/shared/components/navbar/navbar.component.ts
@@ -1,8 +1,18 @@
 import { Component } from '@angular/core';
+import { SessionStorageService } from '../../../core/services/session-storage.service';
 
 @Component({
   selector: 'app-navbar',
   standalone: false,
   templateUrl: './navbar.component.html',
 })
-export class Navbar {}
+export class Navbar {
+
+  constructor(
+    private sessionStorage:SessionStorageService,
+  ) {}
+
+  isLoggedIn(): boolean {
+    return this.sessionStorage.getItem('token') !== null;
+  }
+}
